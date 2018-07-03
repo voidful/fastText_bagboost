f7dbd779a5a82e9202e793948be670c1527f0357
Eric Chung <lam970805@gmail.com>
2018-07-03 15:49:34 +0800

    pixnet tidy

--- training_data/merge_training_data.py
+++ training_data/merge_training_data.py
@@ -5,9 +5,10 @@ data_x = []
 data_y = []
 testing_ratio = 0.3
 
-
-def main():
-    with open('./taipei/Taipei_QA.txt', "r", encoding='utf-8') as f:
+folder = './pixnet/'
+origin_file_name = 'Pixnet_QA.txt'
+if __name__ == '__main__':
+    with open(folder + origin_file_name, "r", encoding='utf-8') as f:
         for i in f.readlines():
             label = re.split(" ", i)[0]
             question = re.split(" ", i)[1].rstrip()
@@ -15,15 +16,12 @@ def main():
             data_x.append(processed_question)
             data_y.append(label)
 
-    x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.3)
+    x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.7)
 
-    with open('./taipei/training.data', "w", encoding='utf-8') as f:
+    with open(folder + 'training.data', "w", encoding='utf-8') as f:
         for i in range(len(x_train)):
             f.write(y_train[i] + " " + x_train[i] + "\n")
 
-    with open('./taipei/testing.data', "w", encoding='utf-8') as f:
+    with open(folder + 'testing.data', "w", encoding='utf-8') as f:
         for i in range(len(x_test)):
             f.write(y_test[i] + " " + x_test[i] + "\n")
-
-
-main()
